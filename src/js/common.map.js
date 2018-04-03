3b505843b56ba46167a5a49833270b047424ae48
fuyanan <meihuo_fyn@163.com>
2018-04-03 19:05:05 +0800

    修改

--- src/js/common.map.js
+++ src/js/common.map.js
@@ -1,6 +1,8 @@
 document.write('<script type="text/javascript" src="/js/jquery/jquery.min.js"></script>');
 document.write('<script type="text/javascript" src="/js/vue/vue.js"></script>');
-
+document.write('<script type="text/javascript" src="/js/vue/vue-router.js"></script>');
+document.write('<script src="/js/apps/components/nav.js"></script>');
+	
 document.write('<link rel="stylesheet" href="/css/reset.css"></link>');
 document.write('<link rel="stylesheet" href="/css/main.css"></link');
 
